f0ac4bc9b8d24429e12ba9c045cde4a9b3ea1027
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-07-10 00:00:14 -0400

    updated var URL

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code here
-var URL = 'b8654bb0c6ea71c3b1056ab5f5581615'
-var weathDiv = document.getElementById('weather')
+var URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+var weatherDiv = document.getElementById('weather')
 var form = document.querySelector('form')
+
